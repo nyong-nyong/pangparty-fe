864b2e225d3fa3f1d7c4ca68642a6fc864a59bca
hanbeulYou <hanbeul.you@gmail.com>
2023-01-22 14:48:32 +0900

    S08P12A306-201 Feat: Photo Album 컴포넌트 생성-1

---
+++ src/components/PhotoAlbum/PhotoAlbum.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+export default function PhotoAlbum() {
+
+  const dummyPhotos = [
+    {
+      uid: 1,
+      event_uid: 1,
+      photo_url: 'https://img.jbox.co.kr/UpFiles/Content/Image/34155/shot_l/34155_8.jpg',
+      created_time: '',
+      taken_time: '',
+      taken_lat: '',
+      taken_lng: '',
+    },
+    {
+      uid: 2,
+      event_uid: 2,
+      photo_url: '',
+      created_time: '',
+      taken_time: '',
+      taken_lat: '',
+      taken_lng: '',
+    },
+    {
+      uid: 2,
+      event_uid: 2,
+      photo_url: '',
+      created_time: '',
+      taken_time: '',
+      taken_lat: '',
+      taken_lng: '',
+    },
+  ];
+
+  return (
+    <div>
+      <h1>PhotoAlbum</h1>
+    </div>
+  )
+}
--- src/pages/IntroEventPage.js
+++ src/pages/IntroEventPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PhotoAlbum from '../components/PhotoAlbum/PhotoAlbum';
 
 export default function IntroEventPage() {
   return (
@@ -7,6 +8,8 @@ export default function IntroEventPage() {
       <h1>여기는 이벤트 소개 페이지입니다.</h1>
 
       <Link to="/piece">✏️롤링페이퍼 쓰기 버튼</Link>
+
+      <PhotoAlbum/>
     </div>
   );
 }
