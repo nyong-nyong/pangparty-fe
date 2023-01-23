1ccd19e621c84530e037788792299f9b7c7213bd
hanbeulYou <hanbeul.you@gmail.com>
2023-01-23 15:19:45 +0900

    S08P12A306-211 Feat: Photo File 서버 전송 기능 구현-1

--- src/components/PhotoAlbum/PhotoUpload.js
+++ src/components/PhotoAlbum/PhotoUpload.js
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import React, { useState, useRef } from 'react'
+import styled from 'styled-components';
 
 export default function PhotoUpload() {
 
@@ -14,16 +16,41 @@ export default function PhotoUpload() {
     }
   }
 
+  const submitPhotoFile = async (e) => {
+    e.preventDefault();
+    const photo = photoRef.current.files[0];
+
+    if(photo) {
+      const formData = new FormData();
+      formData.append('requests', photo)
+
+      for(const data of formData) console.log(data)
+
+      await axios({
+        method: 'post',
+        url: 'http://localhost:4000/requests',
+        data: formData,
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+    }
+  }
+
+
   return (
-    <form>
-      <label htmlFor={'photoUpload'}>사진 업로드</label>
+    <form onSubmit={submitPhotoFile}>
+      <PhotoUploadLabel htmlFor={'photoUpload'}>사진 업로드</PhotoUploadLabel>
       <input
         type={'file'}
         accept={'image/*'}
         id={'photoUpload'}
         onChange={savePhotoFile}
         ref={photoRef}
+        style={{ display: "none"}}
       />
+      <button>전송</button>
+      <br/>
       <img
         src={photoFile}
         alt={"업로드된 사진"}
@@ -31,3 +58,12 @@ export default function PhotoUpload() {
     </form>
   )
 }
+
+const PhotoUploadLabel = styled.label`
+  margin: 5px 0 20px 0;
+  font-weight: bold;
+  font-size: 13px;
+  color: #0095f6;
+  display: inline-block;
+  cursor: pointer;
+`
