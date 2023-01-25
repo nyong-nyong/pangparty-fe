a6cbb6ce906790a9c8ca3678b65c56d5719ab7c9
gimgyuyeon <8080claire@naver.com>
2023-01-25 11:23:46 +0900

    S08P12A306-186 remove: 이미지 파일 삭제(기존 컴포넌트 방식변경)

--- src/pages/PieceListPage.js
+++ src/pages/PieceListPage.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Modal from "../components/common/Modal";
-import StickerList from "../components/Sticker/StickerList";
 import StickerLists from "../components/Sticker/StickerLists";
 
 // 완성된 롤링페이퍼 페이지
@@ -17,9 +16,6 @@ export default function PieceListPage() {
       <h1>완성된 롤링페이퍼 페이지</h1>
       <div style={{ width: "100%", height: "500px" }}></div>
       <button onClick={showModal}>스티커 붙이기</button>
-      {/* {modalOpen && (
-        <Modal setModalOpen={setModalOpen} InnerComponent={StickerList} />
-      )} */}
       {modalOpen && (
         <Modal setModalOpen={setModalOpen} InnerComponent={StickerLists} />
       )}
