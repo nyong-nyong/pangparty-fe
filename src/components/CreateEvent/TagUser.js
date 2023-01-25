9998e9de28b6b2fce9d4c9bfeb0ecad3d0058e79
gimgyuyeon <8080claire@naver.com>
2023-01-25 17:52:37 +0900

    S08P12A306-194 feature: 검색 dummy json 불러오기 추가

--- src/components/CreateEvent/TagUser.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function TagUser() {
-  return (
-    <div>TagUser</div>
-  )
-}
--- src/pages/CreateEventPage.js
+++ src/pages/CreateEventPage.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
+import TagMember from "../components/CreateEvent/TagMember";
 
 export default function CreateEventPage() {
+
   return (
-    <div>이벤트 생성페이지</div>
-  )
+    <div>
+      <h1>이벤트 생성페이지</h1>
+      <TagMember />
+    </div>
+  );
 }
