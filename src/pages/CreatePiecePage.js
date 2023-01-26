862316b3bab45cc9b346c61c9e5a4714455e3ac6
devsomda <devsomda@gmail.com>
2023-01-26 13:21:04 +0900

    S08P12A306-299 chore: 미사용 styled component 주석처리

--- src/pages/CreatePiecePage.js
+++ src/pages/CreatePiecePage.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
-import styled from "styled-components"
+// import styled from "styled-components"
 import RpThemeChange from "../components/CreatePiece/RpThemeChange";
 import CreatedPiece from "../components/CreatePiece/CreatedPiece";
 import "../components/CreatePiece/CreatePiece.css";
