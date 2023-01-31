1a330381626a1d6a8614adc31def381f842840f9
dbgksquf05 <dbgksquf05@naver.com>
2023-01-31 17:55:53 +0900

    S08P12A306-206 feat: 사진에 댓글 기능 구현-4

--- src/components/PhotoAlbum/PhotoDetail.js
+++ src/components/PhotoAlbum/PhotoDetail.js
@@ -1,13 +1,29 @@
 import React from 'react'
 import PhotoCommentList from './PhotoCommentList'
 import PhotoLikes from './PhotoLikes'
+import styled from 'styled-components';
 
 export default function PhotoDetail({photoSelected, setPhotoSelected, setModalOpen}) {
+  const clickHandle = () => {
+    setModalOpen(false);
+  }
+
   return (
-    <div>
-      {photoSelected.uid}
+    <Modal>
+      <span onClick={clickHandle}>X</span>
+      <img src={photoSelected.mediaUrl} width="300px" height="200px"></img>
       <PhotoLikes />
       <PhotoCommentList />
-    </div>
+    </Modal>
   )
 }
+
+const Modal = styled.div`
+  position: absolute;
+  top: 0px;
+  width: 300px;
+  height: 500px;
+  border: 1px solid black;
+  background-color: grey;
+  z-index: 100;
+`
