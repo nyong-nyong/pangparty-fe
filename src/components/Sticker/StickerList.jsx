5033028e58d78aa4bb4b00d9307c53e3eacacb25
gimgyuyeon <8080claire@naver.com>
2023-02-01 10:20:02 +0900

    S08P12A306-199 chore: movable 설치

--- src/components/Sticker/StickerList.jsx
+++ src/components/Sticker/StickerList.jsx
@@ -1,36 +1,51 @@
 import { useState, useEffect } from "react";
-// import Draggable from "react-draggable";
 import axios from "../../api/axios";
 import requests from "../../api/requests";
 
 export default function StickerList() {
   const [stickerList, setStickerList] = useState([]);
 
-  useEffect(() => {
+  // // 최초 랜더링시 실행
+  // useEffect(() => {
+  //   async function getSticker() {
+  //     await axios
+  //       .get(requests.fetchStickers)
+  //       .then((response) => {
+  //         setStickerList(response.data.stickers);
+  //       })
+  //       .catch((e) => {
+  //         console.log(e);
+  //       });
+  //   }
+  //   getSticker();
+  // });
+
+  // 최초 랜더링시 실행
+  const onClick = () => {
     async function getSticker() {
       await axios
         .get(requests.fetchStickers)
         .then((response) => {
           setStickerList(response.data.stickers);
-          console.log(response.data);
         })
         .catch((e) => {
           console.log(e);
         });
     }
     getSticker();
-  });
+  };
 
   if (!stickerList) return <div>...loading</div>;
 
   return (
     <div>
       <h1>⭐스티커 붙이기⭐</h1>
-      {/* <Draggable> */}
+      <button type="button" onClick={onClick}>
+        스티커 불러오기
+      </button>
       {stickerList.map((sticker) => {
         return <img key={sticker.uid} src={sticker.url} alt="" width="100px" />;
       })}
-      {/* </Draggable> */}
     </div>
   );
 }
