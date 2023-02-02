f8f78781aea7de7539c140e885829a7d4351caee
hanbeulYou <hanbeul.you@gmail.com>
2023-02-03 01:44:20 +0900

    S08P12A306-403-fe-add-search-components-with-api chore: 컴포넌트 파일 생성

--- src/components/Search/SearchResults.jsx
+++ src/components/Search/SearchResults.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { useRecoilValue } from 'recoil'
-import { searchResultsState } from 'recoils/search/Atoms'
+import { searchResultsState, searchTypeState } from 'recoils/search/Atoms'
 
 export default function SearchResults() {
+  const type = useRecoilValue(searchTypeState)
   const searchResults = useRecoilValue(searchResultsState);
 
+  // type별로 map key 변경해야함
+  // 검색 결과에 따른 컴포넌트들 만들자.. ㅎㅎ
+
+  //figma의 자동완성 관련 이야기 물어보기
   return (
     <div>
       { searchResults.length > 0 ? 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
-import SearchBar from "components/Search/SearchBar";
-import SearchResults from "components/Search/SearchResults";
-import SearchType from "components/Search/SearchType";
+// import SearchBar from "components/Search/SearchBar";
+// import SearchResults from "components/Search/SearchResults";
+// import SearchType from "components/Search/SearchType";
 import { Link } from "react-router-dom";
 
 // 우리 메인 홈화면
@@ -9,9 +9,9 @@ export default function HomePage() {
   return (
     <div>
       <h1>여기가 Home 입니다.</h1>
-      <SearchBar/>
+      {/* <SearchBar/>
       <SearchType/>
-      <SearchResults/>
+      <SearchResults/> */}
       <Link to="/event/intro">이벤트 소개페이지 바로가기</Link>
       <Link to="/event/create">
         <button type="button">🎉이벤트 만들기🎉</button>
