f09433e3d26717e0d785aa7887cb629ef89f3a31
gimgyuyeon <8080claire@naver.com>
2023-02-09 21:34:19 +0900

    S08P12A306-593 feat: eventPage 병합 및 컴포넌트 분리작업 완료

---
+++ .history/src/components/CreateEvent/IntroHeader_20230209202012.jsx
@@ -0,0 +1,5 @@
+function IntroHeader() {
+  return <div></div>;
+}
+
+export default IntroHeader;
---
+++ .history/src/components/CreateEvent/IntroHeader_20230209202201.jsx
@@ -0,0 +1,25 @@
+import Pang from "../components/CreateEvent/Pang";
+
+function IntroHeader() {
+  return (
+    <div>
+      <div className="dDayInfoBox">
+        <div className="dDayBox">D-day</div>
+        <p className="dDayDate">{eventInfo && eventInfo.dDay}</p>
+      </div>
+      <header className="header">{eventInfo && eventInfo.eventName}</header>
+      <div className="Pang">
+        <Pang eventUid={params.eventId} />
+        <div className="banner">
+          <div className="bannerContentBox">
+            <p className="bannerContent">
+              {eventInfo && eventInfo.introduction}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default IntroHeader;
