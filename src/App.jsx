fd01f853e6f0b15092cbd16ddc43840b099ca7d5
gimgyuyeon <8080claire@naver.com>
2023-02-12 03:07:22 +0900

    S08P12A306-602 style: loginPage css-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-/* eslint-disable no-unused-vars */
-import { useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { CookiesProvider } from "react-cookie";
 import Routers from "./pages/Routers";
-import LoginPage from "./pages/LoginPage";
 import NavBar from "./components/common/Navbar";
 import Footbar from "./components/common/Footbar";
+import "./styles/App.scss";
 
 // 여기는 완전 최상위 컴포넌트
 // 완성된 페이지들만 뿌려주세요.
 function App() {
-  const [loginPage, setLoginPage] = useState(true);
   return (
     <div className="appContainer">
       <RecoilRoot>
         <CookiesProvider>
           <BrowserRouter>
-            {loginPage ? (
-              <LoginPage />
-            ) : (
-              <>
-                <NavBar />
+            <div className="wrapper">
+              <NavBar />
+              <div className="contentWrapper">
                 <Routers />
-                <Footbar />
-              </>
-            )}
+              </div>
+            </div>
+            <Footbar />
           </BrowserRouter>
         </CookiesProvider>
       </RecoilRoot>
