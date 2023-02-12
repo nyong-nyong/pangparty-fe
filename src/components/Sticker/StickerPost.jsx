748e831df3fad0741cddca32057a41dd4b4c5b9a
devsomda <dev.somda@gmail.com>
2023-02-12 22:25:42 +0900

    S08P12A306-603 feat: sticker api body 수정

--- src/components/Sticker/StickerPost.jsx
+++ src/components/Sticker/StickerPost.jsx
@@ -26,28 +26,35 @@ export default function StickerPost({ eventUid, rpUid }) {
     e.preventDefault();
 
     const stickerValue = document.querySelector(".moveable");
-    console.log(stickerValue.style.cssText);
-    console.log(stickerValue);
+    const stickerCssAllList = stickerValue.style.cssText.split(";");
+    // console.log(stickerCssAllList)
 
-    // const topLoc = stickerValue.style.top  px제거 -> INt변환
+    const lefLoc = stickerCssAllList[3].slice(7, -2);
+    const topLoc = stickerCssAllList[3].slice(6, -2);
+    const angleAndScaleInfo = stickerCssAllList[5].split(" ");
+    const angle = angleAndScaleInfo[2].slice(7, -4);
+    const scale = angleAndScaleInfo[3].slice(7, -1);
 
+    //position: absolute; width: 100px; height: 100px; left: 89px; top: -2px; transform: rotate(0deg) scaleX(1) scaleY(1) matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);
     const saveInfo = {
       memberId: user, // "example",
       stickerUid: sticker.uid, // 1,
-      leftLoc: stickerValue.left, // 0,
-      topLoc: stickerValue.top, // 0,
+      leftLoc: lefLoc, // 0,
+      topLoc: topLoc, // 0,
       zIndex: "100", // "1000",
-      angle: 0.0, // 0.0,
-      scale: 0.0 // 0.00  
-    }
-
+      angle: angle, // 0.0,
+      scale: scale // 0.00
+    };
 
     async function sticker() {
       await axios
-        .post(requests.events.rollingPaper.postSticker(eventUid, rpUid))
+        .post(
+          requests.events.rollingPaper.postSticker(eventUid, rpUid),
+          saveInfo
+        )
         .then((res) => {
           // POST 완료하면 sticker 상태 다시 reset하기
-          setSticker(undaefined);
+          setSticker(undefined);
           console.log(res);
         })
         .catch((error) => {
