367ab88db4a8ae4e396fb3ea1f243bd4ce4ffc92
devsomda <dev.somda@gmail.com>
2023-02-14 01:02:50 +0900

    S08P12A306-708 feat: mypage badge get api 적용

--- src/components/MyPage/Badges.jsx
+++ src/components/MyPage/Badges.jsx
@@ -6,6 +6,7 @@ import useAuth from "../../hooks/useAuth";
 
 export default function Badges() {
   const [badgeList, setBadgeList] = useState(undefined);
+  const [isOpened, setIsOpened] = useState(false);
 
   const auth = useAuth();
   const [user, setUser] = useState("");
@@ -18,27 +19,48 @@ export default function Badges() {
         requests.profile.getProfileBadges(`${user}`)
       );
       setBadgeList(request.data);
+      console.log(request.data);
     }
     fetchData();
   }, [user]);
 
+  const openDiscription = () => {
+    setIsOpened(!isOpened);
+  };
+
   return (
-    <div className="badgeContainer">
-      {badgeList &&
-        badgeList.badges.map((badge) => {
-          if (badge) {
-            return (
-              // 디자인 MyPage.css 적용
-              <div key={badge.badgeUid}>
-                <div className="badgeImgContainer">
-                  <img className="badgeImg" src={badge.imgUrl} alt="badge" />
-                  <p>{badge.badgeName}</p>
+    <div className="profilebottomContainer">
+      <div className="badgeContainer">
+        <button type="button" onClick={openDiscription}>
+          설명보기
+        </button>
+        {badgeList &&
+          badgeList.memberBadges.map((badge) => {
+            if (badge) {
+              return (
+                // 디자인 MyPage.css 적용
+                <div key={badge.uid}>
+                  <div className="badgeImgContainer">
+                    <img
+                      className="badgeImg"
+                      src={
+                        badge.hasBadge ? badge.trueImgUrl : badge.falseImgUrl
+                      }
+                      alt="badge"
+                    />
+                    <p className="badgeName">{badge.badgeName}</p>
+                    {isOpened ? (
+                      <div className="rightContainer">
+                        <p>{badge.hasBadge ? badge.badgeCondition : null}</p>
+                      </div>
+                    ) : null}
+                  </div>
                 </div>
-              </div>
-            );
-          }
-          return null;
-        })}
+              );
+            }
+            return null;
+          })}
+      </div>
     </div>
   );
 }
