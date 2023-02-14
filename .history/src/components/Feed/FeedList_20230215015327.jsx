39081fc5edb017da0499c4f19b106ac85e3f91c2
gimgyuyeon <8080claire@naver.com>
2023-02-15 03:29:26 +0900

    S08P12A306-710 feat: feed like dislike 기능 추가

---
+++ .history/src/components/Feed/FeedList_20230215015327.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable object-shorthand */
+/* eslint-disable no-unused-vars */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { detailFeedState } from "../../recoils/Feed/Atoms";
+import axios from "../../api/axios";
+import requests from "../../api/requests";
+import "./FeedList.scss";
+import useAuth from "../../hooks/useAuth";
+import SearchEventResult from "../Search/SearchEvent";
+import Feed from "./Feed";
+
+export default function FeedList() {
+  const [feedList, setFeedList] = useState([]);
+  const [detailFeed, setDetailFeed] = useRecoilState(detailFeedState);
+
+  // 한별
+  const auth = useAuth();
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    setUser(auth.user);
+  }, []);
+
+  // localhost
+  const page = 0;
+  const size = 30;
+  useEffect(() => {
+    async function getFeed() {
+      await axios
+        .get(requests.feed.getFeed(page, size))
+        .then((response) => {
+          setFeedList(response.data.feed);
+          // console.log(response.data.feed);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    }
+    getFeed();
+  }, []);
+
+  return (
+    <div className="feedWrapper">
+      {feedList ? (
+        feedList.map((feed) => {
+          if (!feed) return null;
+          return (
+            <div key={feed.uid}>
+              <Link
+                to={`/feed/${feed.uid}`}
+                onClick={() => {
+                  setDetailFeed(feed);
+                }}
+              >
+                <Feed feed={feed} />
+              </Link>
+              <ul>
+                {feed.event ? <SearchEventResult event={feed.event} /> : null}
+              </ul>
+            </div>
+          );
+        })
+      ) : (
+        <div />
+      )}
+    </div>
+  );
+}
---
+++ .history/src/components/Feed/Feed_20230215011133.jsx
@@ -0,0 +1,116 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
+import profile from "../../assets/profile.svg";
+import Icon from "../common/Icon";
+import "./FeedList.scss";
+import axios from "../../api/axios";
+import requests from "../../api/requests";
+import useAuth from "../../hooks/useAuth";
+
+// FeedList에서 feed 객체를 recoil로부터 받아옴(한 개의 게시물)
+// 피드(게시물) 상세페이지
+// axios로 현재 피드(게시물) id를 받아서 넘겨주면 like랑 comment 받아오기
+// like ->  getPostDetail
+// comment -> getPostComment
+
+// 있어야 할 것 -> like(T/F, cnt), comment(T/F, cnt)
+
+export default function Feed({ feed }) {
+  const [isLiked, setIsLiked] = useState(false);
+  const [likeCnt, setLikeCnt] = useState(0);
+  const [profileImgUrl, setProfileImgUrl] = useState("");
+
+  // 로그인 정보
+  const auth = useAuth();
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    setUser(auth.user);
+  }, []);
+
+  useEffect(() => {
+    // 좋아요 갯수 T/F 받아오기
+
+    async function fetchData() {
+      if (!feed) return;
+      const request = await axios
+        .get(requests.posts.getPostDetail(`${feed.uid}`))
+        .then((response) => {
+          setIsLiked(response.data.isLiked);
+          setLikeCnt(response.data.likeCount);
+          // console.log(isLiked);
+          // console.log(likeCnt);
+          console.log(response.data);
+          setProfileImgUrl(response.data.profileImgUrl);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+    fetchData();
+  }, [feed]);
+
+  const likeClickHandler = (e) => {
+    e.preventDefault();
+
+    async function deleteLike() {
+      await axios
+        .delete(requests.events.introEvent.deletePang(eventUid))
+        .then(setPangCnt(pangCnt - 1), setIsPang(!isPang))
+        .catch((err) => console.log(err));
+    }
+  }
+
+  // 시간 계산 함수
+  function timeForToday(value) {
+    const today = new Date();
+    const timeValue = new Date(value);
+    const betweenTime = Math.floor(
+      (today.getTime() - timeValue.getTime()) / 1000 / 60
+    );
+    if (betweenTime < 1) return "방금전";
+    if (betweenTime < 60) {
+      return `${betweenTime}분전`;
+    }
+    const betweenTimeHour = Math.floor(betweenTime / 60);
+    if (betweenTimeHour < 24) {
+      return `${betweenTimeHour}시간전`;
+    }
+    const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+    if (betweenTimeDay < 365) {
+      return `${betweenTimeDay}일전`;
+    }
+    return `${Math.floor(betweenTimeDay / 365)}년전`;
+  }
+
+  return (
+    <div className="feedContainer">
+      <div className="feedMember">
+        {profileImgUrl ? (
+          <img src={profileImgUrl} alt="프로필사진" />
+        ) : (
+          <img src={profile} alt="프로필기본사진" />
+        )}
+        <div className="titleAndMember">
+          <p className="feedTitle">{feed?.title}</p>
+          <p className="feedId">@{feed?.memberId}</p>
+        </div>
+      </div>
+      <div className="feedContent">
+        <p>{feed?.content}</p>
+        <p className="feedTime">{timeForToday(feed?.createTime)}</p>
+      </div>
+      <div>
+        <div>
+          <div onClick={likeClickHandler}>
+            <Icon img="like" isActive={isLiked} />
+          </div>
+          <div style={isLiked ? { color: "#FF7A5C" } : { color: "#6B6B6B" }}>
+            {likeCnt}
+          </div>
+        </div>
+        {/* CSS 추후 수정 및 추가 예정 */}
+        <Icon img="comment" />
+      </div>
+    </div>
+  );
+}
