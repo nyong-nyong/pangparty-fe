8d0770b4975004abc1068016a1ad3e60ad5638e1
gimgyuyeon <8080claire@naver.com>
2023-02-14 10:57:21 +0900

    S08P12A306-710 feat: feedPage event Linking

--- src/components/Feed/FeedList.jsx
+++ src/components/Feed/FeedList.jsx
@@ -1,27 +1,35 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { Profiler, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "../../api/axios";
 import requests from "../../api/requests";
 import "./FeedList.scss";
 import profile from "../../assets/profile.svg";
+import useAuth from "../../hooks/useAuth";
+import SearchEventResult from "../Search/SearchEvent";
 
 export default function FeedList() {
   const [feedList, setFeedList] = useState([]);
-  const navigate = useNavigate();
+
+  // 한별
+  const auth = useAuth();
+  const [user, setUser] = useState("");
+  useEffect(() => {
+    setUser(auth.user);
+  }, []);
 
   // localhost
-  const page = 1;
-  const limit = 3;
+  const page = 0;
+  const size = 30;
   useEffect(() => {
     async function getFeed() {
       await axios
-        .get(requests.feed.getFeed(page, limit))
+        .get(requests.feed.getFeed(page, size))
         .then((response) => {
-          setFeedList(response.data.posts);
-          console.log(response.data);
+          setFeedList(response.data.feed);
+          console.log(response.data.feed);
         })
         .catch((e) => {
           console.log(e);
@@ -30,16 +38,9 @@ export default function FeedList() {
     getFeed();
   }, []);
 
-  // const handleClickPost = (e, post) => {
-  //   e.preventDefault();
-  //   console.log(post.uid);
-  //   navigate(`/${post.uid}`);
-  // };
-
   function timeForToday(value) {
     const today = new Date();
     const timeValue = new Date(value);
-
     const betweenTime = Math.floor(
       (today.getTime() - timeValue.getTime()) / 1000 / 60
     );
@@ -47,41 +48,42 @@ export default function FeedList() {
     if (betweenTime < 60) {
       return `${betweenTime}분전`;
     }
-
     const betweenTimeHour = Math.floor(betweenTime / 60);
     if (betweenTimeHour < 24) {
       return `${betweenTimeHour}시간전`;
     }
-
     const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
     if (betweenTimeDay < 365) {
       return `${betweenTimeDay}일전`;
     }
-
     return `${Math.floor(betweenTimeDay / 365)}년전`;
   }
 
   return (
     <div className="feedWrapper">
       {feedList ? (
-        feedList.map((post) => {
+        feedList.map((feed) => {
+          if (!feed) return null;
           return (
-            // <div key={post.uid} onClick={(e) => handleClickPost(e, post)}>
-            <Link to={`/feed/${post.uid}`}>
-              <div key={post.uid}>
+            // <div key={feed.uid} onClick={(e) => handleClickfeed(e, feed)}>
+            <Link to={`/feed/${feed.uid}`}>
+              <div key={feed.uid}>
                 <div className="feedContainer">
                   <div className="feedMember">
                     <img src={profile} alt="" />
                     <div className="titleAndMember">
-                      <p className="feedTitle">게시글 제목</p>
-                      <p className="feedId">@{post.id}</p>
+                      <p className="feedTitle">{feed.title}</p>
+                      <p className="feedId">@{feed.memberId}</p>
                     </div>
                   </div>
                   <div className="feedContent">
-                    <p>{post.content}</p>
-                    <p className="feedTime">{timeForToday(post.createTime)}</p>
+                    <p>{feed.content}</p>
+                    <p className="feedTime">{timeForToday(feed.createTime)}</p>
                   </div>
                 </div>
+                <ul>
+                  {feed.event ? <SearchEventResult event={feed.event} /> : null}
+                </ul>
               </div>
             </Link>
           );
