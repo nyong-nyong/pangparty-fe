2f55858ce0f4352d42f637ebbf1fb16508eb3c3f
devsomda <dev.somda@gmail.com>
2023-02-16 01:13:14 +0900

    S08P12A306-718 feat: gift detail api 등록

--- src/pages/PangPartyRecapPage/GiftDetailPage.jsx
+++ src/pages/PangPartyRecapPage/GiftDetailPage.jsx
@@ -3,46 +3,47 @@ import { useParams } from "react-router-dom";
 import axios from "../../api/axios";
 import requests from "../../api/requests";
 import "../../styles/EventDetailPage.scss";
-import GiftRpList from "../../components/Gift/GiftRpList";
-import PhotoAlbum from "../../components/PhotoAlbum/PhotoAlbum";
-import IntroHeader from "../../components/CreateEvent/IntroHeader";
-import IntroHashTag from "../../components/CreateEvent/IntroHashTag";
+// import GiftRpList from "../../components/Gift/GiftRpList";
+// import PhotoAlbum from "../../components/PhotoAlbum/PhotoAlbum";
+// import IntroHeader from "../../components/CreateEvent/IntroHeader";
+// import IntroHashTag from "../../components/CreateEvent/IntroHashTag";
 import useAuth from "../../hooks/useAuth";
 
 export default function GiftDetailPage() {
   const [eventInfo, setEventInfo] = useState(undefined);
-  const [isPart, setIsPart] = useState(false);
+  // const [isPart, setIsPart] = useState(false);
   const auth = useAuth();
   const [user, setUser] = useState("");
 
   const params = useParams();
 
   useEffect(() => {
-    const id = params.eventId;
-    async function fetchData() {
-      const request = await axios.get(
-        requests.events.introEvent.eventItroAll(id)
-      );
-      // console.log(request.data);
-      setEventInfo(request.data);
-      setIsPart(request.data.isParticipant);
-      // console.log(request.data.isParticipant);
-    }
     setUser(auth.user);
-    // console.log(user);
+    const id = params.eventId;
+
+    async function fetchData() {
+      await axios
+        .get(requests.events.getPangExportAll(id))
+        .then((res) => {
+          setEventInfo(res.data);
+          console.log(res.data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
     fetchData();
-    // const rpUid = response.data.rollingPaperUid;
-    console.log(eventInfo);
-    console.log(params);
   }, [user]);
 
   return (
     <div className="detailContainer">
-      <IntroHeader eventInfo={eventInfo} params={params} />
+      <p>test</p>
+      {eventInfo ? <p>받았다</p> : <p>no</p>}
+      {/* <IntroHeader eventInfo={eventInfo} params={params} />
       <GiftRpList eventInfo={eventInfo} params={params} />
       <IntroHashTag eventInfo={eventInfo} />
       <p className="albumTitle">@{eventInfo && eventInfo.targetId} 과의 추억</p>
-      <PhotoAlbum isPart={isPart} eventUid={params.eventId} />
+      <PhotoAlbum isPart={isPart} eventUid={params.eventId} /> */}
     </div>
   );
 }
