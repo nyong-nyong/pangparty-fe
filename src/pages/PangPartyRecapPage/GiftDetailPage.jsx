9e4cdfe1cfacf731023db1035f59b29cb5332f71
devsomda <dev.somda@gmail.com>
2023-02-16 02:31:54 +0900

    S08P12A306-718 feat: pangparty giftdetailpage api 적용

--- src/pages/PangPartyRecapPage/GiftDetailPage.jsx
+++ src/pages/PangPartyRecapPage/GiftDetailPage.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import JSConfetti from "js-confetti";
 import axios from "../../api/axios";
 import requests from "../../api/requests";
 import "../../styles/EventDetailPage.scss";
 // import GiftRpList from "../../components/Gift/GiftRpList";
 // import PhotoAlbum from "../../components/PhotoAlbum/PhotoAlbum";
-// import IntroHeader from "../../components/CreateEvent/IntroHeader";
-// import IntroHashTag from "../../components/CreateEvent/IntroHashTag";
+import IntroHeader from "../../components/CreateEvent/IntroHeader";
+import IntroHashTag from "../../components/CreateEvent/IntroHashTag";
 import useAuth from "../../hooks/useAuth";
 
 export default function GiftDetailPage() {
@@ -35,15 +36,35 @@ export default function GiftDetailPage() {
     fetchData();
   }, [user]);
 
+  const jsConfetti = new JSConfetti();
+
+  const confettiPang = () => {
+    jsConfetti.addConfetti({
+      confettiColors: [
+        "#ff0a54",
+        "#ff477e",
+        "#ff7096",
+        "#ff85a1",
+        "#fbb1bd",
+        "#f9bec7",
+      ],
+      // emojis: ["💖", "✨", "🥳", "🎊", "🍰", "🦋"],
+      confettiRadius: 5,
+      confettiNumber: 100,
+    });
+  };
+
   return (
-    <div className="detailContainer">
+    <div className="detailContainer" onTouchStart={confettiPang}>
       <p>test</p>
       {eventInfo ? <p>받았다</p> : <p>no</p>}
-      {/* <IntroHeader eventInfo={eventInfo} params={params} />
-      <GiftRpList eventInfo={eventInfo} params={params} />
-      <IntroHashTag eventInfo={eventInfo} />
-      <p className="albumTitle">@{eventInfo && eventInfo.targetId} 과의 추억</p>
-      <PhotoAlbum isPart={isPart} eventUid={params.eventId} /> */}
+      <IntroHeader eventInfo={eventInfo.eventIntroduce} params={params} />
+      {/* <GiftRpList eventInfo={eventInfo} params={params} /> */}
+      <IntroHashTag eventInfo={eventInfo.eventIntroduce} />
+      <p className="albumTitle">
+        @{eventInfo && eventInfo.eventIntroduce.targetId} 과의 추억
+      </p>
+      {/* <PhotoAlbum isPart={isPart} eventUid={params.eventId} /> */}
     </div>
   );
 }
