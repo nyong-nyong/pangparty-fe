cca5689868ea5368fd001f5958e6b99b2249dbc5
devsomda <dev.somda@gmail.com>
2023-02-16 02:02:10 +0900

    S08P12A306-718 style: homepage css update

--- src/components/HomePage/HomeEndEvents.jsx
+++ src/components/HomePage/HomeEndEvents.jsx
@@ -22,18 +22,32 @@ export default function HomeEndEvents() {
   }, []);
   return (
     <div className="homeEventSectionContainer">
-      <div className="homeEventHeader">
-        <img className="checkIcon" />
-        <span>D-day, 오늘 꼭 축하해주세요!</span>
-      </div>
-      <ul>
-        {events
-          ? events.map((event) => {
-              if (!event) return null;
-              return <SearchEventResult key={event.eventUid} event={event} />;
-            })
-          : null}
-      </ul>
+      {events ? (
+        <div>
+          <div className="homeEventHeader">
+            <img className="checkIcon" />
+            <span>D-day, 오늘 꼭 축하해주세요!</span>
+          </div>
+          <ul>
+            {events
+              ? events.map((event) => {
+                  if (!event) return null;
+                  return (
+                    <SearchEventResult key={event.eventUid} event={event} />
+                  );
+                })
+              : null}
+          </ul>
+        </div>
+      ) : (
+        <div className="homeEventHeader">
+          <img className="checkIcon" />
+          <span>
+            오늘은 마감 이벤트가 없는 날이에요. <br /> 새로운 D-Day를 기대해
+            주세요!
+          </span>
+        </div>
+      )}
     </div>
   );
 }
