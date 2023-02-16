3409d37d789c3bf206a514cb2b62004bd215e5f8
hanbeulYou <hanbeul.you@gmail.com>
2023-02-17 01:26:47 +0900

    S08P12A306-722 feat: 회원가입 유효성 검사

--- src/pages/SignUpPage/SignUpEmailPage.jsx
+++ src/pages/SignUpPage/SignUpEmailPage.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable prefer-regex-literals */
 /* eslint-disable no-nested-ternary */
 /* eslint-disable no-case-declarations */
 /* eslint-disable default-case */
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "../../api/axios";
 import requests from "../../api/requests";
@@ -34,9 +36,30 @@ export default function SignUpEmail() {
     password: true,
     passwordCheck: true,
     name: true,
-    imgUrl: true,
     introduction: true,
   });
+
+  const [canSubmit, setCanSubmit] = useState(false);
+
+  useEffect(() => {
+    if (
+      userInfo.id !== "" &&
+      userInfo.email !== "" &&
+      userInfo.password !== "" &&
+      userInfo.passwordCheck !== "" &&
+      userInfo.name !== "" &&
+      isValid.id &&
+      isValid.idDup &&
+      isValid.email &&
+      isValid.emailDup &&
+      isValid.password &&
+      isValid.passwordCheck &&
+      isValid.name &&
+      isValid.introduction) {
+        return setCanSubmit(true);
+      }
+      return setCanSubmit(false);
+    }, [isValid]);
 
   // const [profileImgFile, setProfileImgFile] = useState("");
   // const profileImgRef = useRef();
@@ -51,6 +74,7 @@ export default function SignUpEmail() {
   // };
 
   const emailIsValid = (email) => {
+    if(email.length === 0) return true;
     const emailRegExp =
       /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
     if (emailRegExp.test(email)) return true;
@@ -58,12 +82,13 @@ export default function SignUpEmail() {
   };
 
   const pwIsValid = (password) => {
+    if(password.length === 0) return true;
     if (password.length < 8 || password.length > 20) {
       return false;
     }
     const numberPattern = new RegExp(/[0-9]/);
     const alphabetPattern = new RegExp(/[a-zA-Z]/);
-    const specialPattern = new RegExp(/[~!@#$%^&*()_+|<>?:{}]/);
+    const specialPattern = new RegExp(/[!@#$?]/);
     if (
       !numberPattern.test(password) ||
       !alphabetPattern.test(password) ||
@@ -75,11 +100,13 @@ export default function SignUpEmail() {
   };
 
   const pwCheckIsValid = (password) => {
+    if (password.length === 0) return true;
     if (password !== userInfo.password) return false;
     return true;
   };
 
   const idIsValid = (id) => {
+    if(id.length === 0) return true;
     if (id.length < 2 || id.length > 15) return false;
     const idRegExp = /[0-9a-zA-Z]/;
     if (idRegExp.test(id)) {
@@ -89,6 +116,7 @@ export default function SignUpEmail() {
   };
 
   const nameIsValid = (name) => {
+    if(name.length === 0) return true;
     if (name.length < 2 || name.length > 15) return false;
     return true;
   };
@@ -172,17 +200,6 @@ export default function SignUpEmail() {
 
   const signUpPost = async (e) => {
     e.preventDefault();
-
-    // const formData = new FormData();
-    // // ""에 key 값 넣기
-    // formData.append(
-    //   "data",
-    //   new Blob([JSON.stringify(userInfo)], {
-    //     type: "application/json",
-    //   })
-    // );
-    // if (profileImgFile) formData.append("file", profileImgFile);
-
     await axios
       .post(requests.register, userInfo, {
         headers: {
@@ -190,9 +207,7 @@ export default function SignUpEmail() {
         },
       })
       .then((res) => {
-        console.log(res);
         if (res.status === 201) {
-          // 로그인으로 이동
           navigate("/login");
         }
       })
@@ -217,11 +232,15 @@ export default function SignUpEmail() {
           value={userInfo.email}
           onChange={signupHandler}
           label="이메일"
-          placeholder="example@naver.com"
+          placeholder="example@gmail.com"
           maxLength="320"
         />
       </div>
-      {isValid.email && isValid.emailDup ? null : isValid.email ? (
+      {isValid.email && isValid.emailDup ? (
+        <span className="guideMsg">
+          이메일은 example@gmail.com 형식으로 입력해주세요.
+        </span>
+      ) : isValid.email ? (
         <span className="errorMsg">이미 존재하는 email 입니다.</span>
       ) : (
         <span className="errorMsg">유효하지 않은 email 형식입니다.</span>
@@ -237,7 +256,12 @@ export default function SignUpEmail() {
           maxLength="20"
         />
       </div>
-      {isValid.password ? null : (
+      {isValid.password ? (
+        <span className="guideMsg">
+          비밀번호는 영문, 숫자, 특수문자(!,@,#,$,?)를 포함한
+          8-20자로 구성되어야 합니다.
+        </span>
+      ) : (
         <span className="errorMsg">유효하지 않은 비밀번호 형식입니다.</span>
       )}
 
@@ -266,14 +290,18 @@ export default function SignUpEmail() {
           maxLength="15"
         />
       </div>
-      {isValid.id && isValid.idDup ? null : isValid.id ? (
+      {isValid.id && isValid.idDup ? (
+        <span className="guideMsg">
+          아이디는 영어 대소문자와 숫자를 포함한 2-15자로 구성되어야 합니다.
+        </span>
+      ) : isValid.id ? (
         <span className="errorMsg">이미 존재하는 ID입니다</span>
       ) : (
         <span className="errorMsg">유효하지 않은 ID 형식입니다</span>
       )}
 
       <div className="inputContainer">
-        <p>Name</p>
+        <p>이름</p>
         <input
           id="name"
           type="text"
@@ -283,10 +311,14 @@ export default function SignUpEmail() {
           maxLength="15"
         />
       </div>
-      {isValid.name ? null : <span className="errorMsg">이름 형식 오류</span>}
+      {isValid.name ? (
+        <span className="guideMsg">
+          이름은 한글, 영어 대소문자와 숫자를 포함한 2-15자로 구성되어야 합니다.
+        </span>
+      ) : <span className="errorMsg">이름 형식 오류</span>}
 
-      <div className="loginProfileContainer">
-        {/* <div className="profileImg">
+      {/* <div className="loginProfileContainer">
+        <div className="profileImg">
           <p>프로필 사진</p>
           <input
             type="file"
@@ -299,21 +331,24 @@ export default function SignUpEmail() {
           ) : (
             <img src={profile} alt="기본 프로필 이미지" />
           )}
-        </div> */}
-        <div className="profileIntro">
-          <p>소개</p>
-          <textarea
-            id="introduction"
-            type="text"
-            value={userInfo.introduction}
-            onChange={signupHandler}
-            placeholder="프로필에 표시될 소개 문구를 작성해주세요."
-          />
         </div>
+      </div> */}
+      <div className="profileIntro">
+        <p>소개</p>
+        <textarea
+          id="introduction"
+          type="text"
+          value={userInfo.introduction}
+          onChange={signupHandler}
+          placeholder="프로필에 표시될 소개 문구를 작성해주세요."
+        />
       </div>
-      <Button color="blue-1" type="button" onClick={signUpPost}>
-        가입완료 확인버튼
-      </Button>
+      {canSubmit ? (<Button color="blue-1" type="button" onClick={signUpPost}>
+        가입하기
+      </Button>) : (<Button color="gray-1" type="button">
+        가입하기
+      </Button>)}
+      
     </div>
   );
 }
