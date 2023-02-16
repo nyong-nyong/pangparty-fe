777ea4c98bbe0e0fadcca5d40d93404f362451b5
gimgyuyeon <8080claire@naver.com>
2023-02-16 15:32:20 +0900

    S08P12A306-717 style: feedList 없을 때 LetsPost페이지 추가

--- src/components/Feed/LetsPost.jsx
+++ src/components/Feed/LetsPost.jsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+import cat from "../../assets/grayCat.svg";
+import Button from "../common/Button";
+import "./LetsPost.scss";
+
 export default function LetsPost() {
-  return <div>LetsPost</div>;
+  return (
+    <div className="LetsPostWrapper">
+      <img src={cat} alt="" />
+      <p>아직 작성된 게시글이 없어요</p>
+      <div className="LstsPangBtnContainer">
+        <Link to="/feed/create">
+          <Button color="blue-1">글 쓰러 가기</Button>
+        </Link>
+        <Link to="/search">
+          <Button color="orange-1">팔로우 하러 가기</Button>
+        </Link>
+      </div>
+    </div>
+  );
 }
